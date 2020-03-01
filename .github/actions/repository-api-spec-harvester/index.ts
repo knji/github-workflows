3181d6412f621e937ac59a7ce353c36204e92cd7
Klaus Nji <klaus.nji@sailpoint.com>
2020-03-01 12:15:54 -0600

    attempting to use Typescript

--- .github/actions/repository-api-spec-harvester/index.ts
+++ .github/actions/repository-api-spec-harvester/index.ts
@@ -3,16 +3,18 @@ import {wait} from './wait'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`)
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+        console.debug("Attempting to use Typescript....")
+        const ms: string = core.getInput('milliseconds')
+        core.debug(`Waiting ${ms} milliseconds ...`)
 
-    core.setOutput('time', new Date().toTimeString())
+        core.debug(new Date().toTimeString())
+        await wait(parseInt(ms, 10))
+        core.debug(new Date().toTimeString())
+
+        core.setOutput('time', new Date().toTimeString())
   } catch (error) {
-    core.setFailed(error.message)
+        core.setFailed(error.message)
   }
 }
 
